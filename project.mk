7a3d447516e08f4b4ae6de5bd6c8da7ee251f3c0
Yury Tsarev <yury@upbound.io>
2025-01-22 18:23:41 +0100

    Override XPKG_IGNORE in project.mk
    Signed-off-by: Yury Tsarev <yury@upbound.io>

--- project.mk
+++ project.mk
@@ -2,3 +2,4 @@ PROJECT_NAME := configuration-gitops-flux
 UPTEST_INPUT_MANIFESTS := examples/flux-xr.yaml
 UPTEST_SKIP_IMPORT := true
 UPTEST_SKIP_UPDATE := true
+XPKG_IGNORE ?= .github/workflows/*.yaml,.github/workflows/*.yml,examples/*.yaml,.work/uptest-datasource.yaml,.cache/render/*,test/provider/*
